b4ba01878ceaebe304e106cd68fb1ae79d93f728
“A <00312302@rijnijssel.nl>
2024-05-31 14:12:38 +0200

    controller

--- app/Http/Controllers/tagsController.php
+++ app/Http/Controllers/tagsController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class tagsController extends Controller
 {
-    //
+    public function index(){
+
+        return view('index');
+        
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\tagsController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/index', function () {
-    return view('index');
-});
 
+
+Route::get('index', [tagsController::class, 'index'])->name('index');
 
