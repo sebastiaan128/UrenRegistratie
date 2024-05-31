da3a6063881be77f88676f2d703f327b4354901e
“A <00312302@rijnijssel.nl>
2024-05-31 12:24:07 +0200

    style

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,8 +9,8 @@
 </head>
 <body>
     <div class="w-full flex justify-center items-center h-screen">
-        <div class="w-[40%] bg-slate-600 p-4 rounded-xl">
-            <h1 class="text-center text-white text-2xl font-bold mb-4">Rapportage downloaden</h1>
+        <div class="w-[40%] bg-slate-600 p-20 rounded-xl">
+            <h1 class="text-center text-white text-2xl font-bold mb-6">Rapportage downloaden</h1>
             <div class="grid grid-cols-2">
                 <label for="start-date" class="text-white">Van</label>
                 <input type="date" id="start-date" class="block mx-auto my-2 p-2 rounded-lg border border-gray-300">
@@ -23,7 +23,7 @@
                 </select>
             </div>
             <div class="w-full text-center">
-            <button type="submit" class="w-[35%] bg-blue-500 text-white font-bold py-2 px-4 rounded">
+            <button type="submit" class="w-[35%] bg-blue-500 text-white font-bold mt-10 py-2 px-4 rounded">
                 Download PDF
             </button>
         </div>
