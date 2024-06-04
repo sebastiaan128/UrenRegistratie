82758482e2843c0b2c0e65f211c6a38ebb97a121
Sebastiaan Balfoort <581150@edu.rocmn.nl>
2024-06-04 10:28:25 +0200

    Add index route for tags controller

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,8 @@ use App\Http\Controllers\HomeController;
 
 Route::middleware("auth")->group(function(){
     Route::get('welcome', [HomeController::class, 'index'])->name('home');
+    Route::get('index', [tagsController::class, 'index'])->name('index');
 });
 
-Route::get('index', [tagsController::class, 'index'])->name('index');
 Route::get("/login", [AuthController::class, "login"])->name('login');
 Route::post("/login", [AuthController::class, "loginPost"])->name("login.post");
