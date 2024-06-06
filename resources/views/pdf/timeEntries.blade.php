4aa2b3928211e613464a2b522dde8175c16a37cc
Sebastiaan Balfoort <581150@edu.rocmn.nl>
2024-06-06 10:56:15 +0200

    Update timeEntries.blade.php for PDF generation

--- resources/views/pdf/timeEntries.blade.php
+++ resources/views/pdf/timeEntries.blade.php
@@ -1,9 +1,9 @@
-<!-- resources/views/tags_pdf.blade.php -->
-
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Tags PDF</title>
+    <title>Time Entries PDF</title>
+    <link rel="icon" type="image/svg+xml" href="https://developing.nl/wp-content/uploads/2023/02/icon.png" />
+
     <style>
         body {
             font-family: DejaVu Sans, sans-serif;
@@ -22,22 +22,52 @@
     </style>
 </head>
 <body>
-    <h1>Tags List</h1>
+    <h1>Time Entries List</h1>
     <table>
         <thead>
             <tr>
-                <th>Tag Name</th>
-                <th>Tag Color</th>
+                <th>Datum</th>
+                <th>Taak</th>
+                {{-- <th>Tag</th> --}}
+                <th>Tijd</th>
             </tr>
         </thead>
         <tbody>
-            @foreach($tags as $tag)
-                <tr>
-                    <td>{{ $tag['name'] }}</td>
-                    <td>{{ $tag['team_id'] }}</td>
-                </tr>
-            @endforeach
+            <?php
+                $totalSeconds = 0;
+                foreach ($timeEntries as $entry) {
+                    $milliseconds = $entry->duration;
+                    $seconds = floor($milliseconds / 1000);
+                    $totalSeconds += $seconds;
+            ?>
+            <tr>
+                <td>{{ date('d-m-Y', strtotime($entry->start_date)) }} tot {{ date('d-m-Y', strtotime($entry->end_date)) }}</td>
+                <td>{{ $entry->task }}</td>
+                {{-- <td>{{ $entry->tag }}</td> --}}
+                <td>
+                    <?php
+                        $minutes = floor($seconds / 60);
+                        $hours = floor($minutes / 60);
+
+                        $minutes %= 60;
+                        $seconds %= 60;
+
+                        echo sprintf('%02d:%02d:%02d', $hours, $minutes, $seconds);
+                    ?>
+                </td>
+            </tr>
+            <?php } ?>
         </tbody>
     </table>
+    <?php
+        $totalMinutes = floor($totalSeconds / 60);
+        $totalHours = floor($totalMinutes / 60);
+
+        $totalMinutes %= 60;
+        $totalSeconds %= 60;
+
+        $totalDuration = sprintf('%02d:%02d:%02d', $totalHours, $totalMinutes, $totalSeconds);
+    ?>
+    <p><strong>Totaal duur: </strong><?php echo $totalDuration; ?></p>
 </body>
 </html>
